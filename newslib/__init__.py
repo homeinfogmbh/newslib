ab9b35ab5db4814b1a8497e7ef6ea16e2f9acc76
Richard Neumann <r.neumann@homeinfo.de>
2019-09-19 10:01:46 +0200

    Added Provider to __init__.

--- newslib/__init__.py
+++ newslib/__init__.py
@@ -3,7 +3,8 @@ from different sources into a centralized format.
 """
 
 from newslib.converters import welt_to_dom
+from newslib.enumerations import Provider
 from newslib.orm import CustomerProvider
 
 
-__all__ = ['welt_to_dom', 'CustomerProvider']
+__all__ = ['welt_to_dom', 'Provider', 'CustomerProvider']
