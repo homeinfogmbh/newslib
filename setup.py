9a844861c0dc380a3fcd61dcf58ec588e6c8ac3b
Richard Neumann <mail@richard-neumann.de>
2022-01-19 19:39:28 +0100

    Migrate to setuptools

--- setup.py
+++ setup.py
@@ -1,21 +1,28 @@
 #! /usr/bin/env python3
 
-from distutils.core import setup
+from setuptools import setup
 
 
 setup(
     name='newslib',
     version='latest',
+    use_scm_version={
+        "local_scheme": "node-and-timestamp"
+    },
+    setup_requires=['setuptools_scm'],
     author='HOMEINFO - Digitale Informationssysteme GmbH',
-    author_email='<info at homeinfo dot de>',
+    author_email='<info@homeinfo.de>',
     maintainer='Richard Neumann',
-    maintainer_email='<r dot neumann at homeinfo priod de>',
-    requires=[
-        'functoolsplus',
-        'flask',
-        'timelib',
-        'trias',
-        'hafas',
-        'wsgilib'],
+    maintainer_email='<r.neumann@homeinfo.de>',
+    install_requires=[
+        'ferengi',
+        'filedb',
+        'hinews',
+        'mdb',
+        'newslib',
+        'peewee',
+        'peeweeplus',
+        'wsgilib'
+    ],
     packages=['newslib'],
     description='Multi-source news API.')
