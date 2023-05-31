4efe80a8ac718ff0fe8060e8466ea46381c8c132
Richard Neumann <r.neumann@homeinfo.de>
2023-05-31 09:23:44 +0200

    Introduce requested_articles()

--- newslib/wsgi.py
+++ newslib/wsgi.py
@@ -2,13 +2,14 @@
 
 XXX: For internal use only!
 """
-from typing import Optional, Union
+from typing import Iterator, Union
 
 from flask import request
 
 from filedb import File
 from wsgilib import Application, Binary, JSON, JSONMessage
 
+from newslib.article import Article
 from newslib.filters import articles
 from newslib.functions import list_providers
 
@@ -30,21 +31,16 @@ def _list_providers() -> JSON:
 def _get_articles() -> Union[JSON, JSONMessage]:
     """Return articles of the requested customer and providers."""
 
-    customer, providers = get_customer_and_providers()
-    return JSON([
-        article.to_json() for article
-        in articles(customer, providers)
-    ])
+    return JSON([article.to_json() for article in requested_articles()])
 
 
 @APPLICATION.route('/<sha256sum>', methods=['POST'], strict_slashes=False)
 def _get_image(sha256sum: str) -> Union[Binary, JSONMessage]:
     """Return the requested image for a given customer and providers."""
 
-    customer, providers = get_customer_and_providers()
     images = {
         article.image.sha256sum: article.image
-        for article in articles(customer, providers)
+        for article in requested_articles()
         if article.image
     }
 
@@ -56,13 +52,13 @@ def _get_image(sha256sum: str) -> Union[Binary, JSONMessage]:
     return Binary(file.bytes, filename=file.filename)
 
 
-def get_customer_and_providers() -> tuple[int, Optional[set[str]]]:
-    """Return a tuple of the customer ID and providers set."""
+def requested_articles() -> Iterator[Article]:
+    """Yield articles for the current customer and providers."""
 
     if not (customer := request.json.get('customer')):
         raise JSONMessage('No customer specified.', status=400)
 
     if providers := request.json.get('providers'):
-        return customer, set(providers)
+        return articles(customer, set(providers))
 
-    return customer, None
+    return articles(customer)
